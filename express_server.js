73ba08abddbb4763d2308ed88c62eb3244600aa7
Perry <perrymacdougall@gmail.com>
2019-06-05 00:15:02 +0000

    created a route for handling POST requests, a form for submitting them, and installed a Node package for parsing data from the body of such requests

--- express_server.js
+++ express_server.js
@@ -1,15 +1,22 @@
+// Require and set ExpressJS and defining a port number to listen for requests
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+// Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
+// Object containing key:value pairs of URLs and their shortened counterparts
 let urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+// Requires body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
+// Defining routes for incoming requests
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -23,15 +30,26 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+// Handle POST requests
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
 
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x10000000).toString(36);
+};
+
+// Tells the HTTP server to listen for requests on the port number defined at top
 app.listen(PORT, () => {
   console.log(`Example app listening on ${PORT}!`);
 });
